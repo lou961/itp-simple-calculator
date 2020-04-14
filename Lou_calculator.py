c591c4c8845e72f52d81540e7ff6e5e395c345f7
lou961 <63477843+lou961@users.noreply.github.com>
2020-04-13 20:33:13 -0400

    Update and rename calculator.py to Lou_calculator.py

---
+++ Lou_calculator.py
@@ -0,0 +1,22 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def divide(x, y):
+    If y == 0:
+        return "Invalid value for denominator, cant't divide by 0!"
+    return x / y
+
+def multiply(x, y):
+    return x * y
+
+def square(x):
+    return x * x
+
+def power(x, y):
+    return x ** y
+
+def sqrt(x):
+    return power (x, 1/2)
